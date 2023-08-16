a0c96fb5696d759e5ef85ed1bc64b315350056df
rwaldron <outsideinprofits@gmail.com>
2023-08-16 20:56:55 +1000

    DbSets created

---
+++ taz-hikes.api/Data/TazHikesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using taz_hikes.api.Models;
+
+namespace taz_hikes.api.Data
+{
+	public class TazHikesDbContext : DbContext
+	{
+		public TazHikesDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+		{
+		}
+
+		public DbSet<Difficulty> Difficulties { get; set; }
+		public DbSet<Region> Regions { get; set; }
+		public DbSet<Hike> Hikes { get; set; }
+	}	
+}
+
