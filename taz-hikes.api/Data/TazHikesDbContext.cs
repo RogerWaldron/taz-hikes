75d1bdfdaa5e7833b49afbe28d5068a9ace2be0e
Roger Waldron <87526672+RogerWaldron@users.noreply.github.com>
2023-08-18 02:32:08 +0400

    Database (#2)
    * DbSets created
    
    * initial migration
    
    * database created with command - dotnet ef database update
    
    * fix: initial migration to create tables
    
    * fix: Connection string
    
    * Renamed proect for consistency
    
    ---------
    
    Co-authored-by: rwaldron <outsideinprofits@gmail.com>

---
+++ taz-hikes.api/Data/TazHikesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TazHikes.api.Models;
+
+namespace TazHikes.api.Data
+{
+	public class TazHikesDbContext : DbContext
+	{
+		public TazHikesDbContext(DbContextOptions<TazHikesDbContext> dbContextOptions): base(dbContextOptions)
+		{
+		}
+
+		public DbSet<Difficulty> Difficulties { get; set; }
+		public DbSet<Region> Regions { get; set; }
+		public DbSet<Hike> Hikes { get; set; }
+	}	
+}
+
---
+++ taz-hikes.api/Migrations/20230817051213_InitialCreate.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TazHikes.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Difficulties",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Difficulties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RegionImgUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hikes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DistanceInKm = table.Column<double>(type: "float", nullable: false),
+                    HikeImgUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DifficultyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RegionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hikes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Hikes_Difficulties_DifficultyId",
+                        column: x => x.DifficultyId,
+                        principalTable: "Difficulties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Hikes_Regions_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hikes_DifficultyId",
+                table: "Hikes",
+                column: "DifficultyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hikes_RegionId",
+                table: "Hikes",
+                column: "RegionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hikes");
+
+            migrationBuilder.DropTable(
+                name: "Difficulties");
+
+            migrationBuilder.DropTable(
+                name: "Regions");
+        }
+    }
+}
